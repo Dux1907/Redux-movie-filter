e024a7807fad1516fedfbec94a0c4e0e8178fbf9
Dux <kartikmadaan1907@gmail.com>
2023-10-06 01:47:16 +0530

    Added checks

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,10 +5,12 @@ import MovieListing from "../MovieListing";
 const Home = () => {
   let dispatch = useDispatch();
   const [val, setVal] = useState("");
+  const [loading, setLoading] = useState(false);
   //console.log(val)
   useEffect(() => {
     dispatch(fetchAllMovies("fast"));
     dispatch(fetchAllShows("fast"));
+    setLoading(true);
   }, [dispatch]);
   const get = () => {
     dispatch(fetchAllMovies(val));
@@ -16,24 +18,28 @@ const Home = () => {
   };
   return (
     <div className="input">
-      <input
-        type="text"
-        className="form-control my-3"
-        placeholder="Enter Movie or Series"
-        aria-label="Enter Movie"
-        value={val}
-        onChange={(e) => setVal(e.target.value)}
-        aria-describedby="button-addon2"
-      />
-      <button
-        className="btn btn-outline-light"
-        type="button"
-        id="button-addon2"
-        onClick={get}
-      >
-        Search
-      </button>
-      <MovieListing />
+      {loading && (
+        <>
+          <input
+            type="text"
+            className="form-control my-3"
+            placeholder="Enter Movie or Series"
+            aria-label="Enter Movie"
+            value={val}
+            onChange={(e) => setVal(e.target.value)}
+            aria-describedby="button-addon2"
+          />
+          <button
+            className="btn btn-outline-light"
+            type="button"
+            id="button-addon2"
+            onClick={get}
+          >
+            Search
+          </button>
+          <MovieListing />
+        </>
+      )}
     </div>
   );
 };
